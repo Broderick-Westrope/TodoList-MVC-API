717d427c96693c51c35587b67ea12eb16d0fcffc
Broderick Westrope <broderickwestrope@gmail.com>
2023-05-18 11:28:23 +1000

    Add Project model.

--- TodoList.MVC.API/Models/Project.cs
+++ TodoList.MVC.API/Models/Project.cs
@@ -1,6 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TodoList.MVC.API.Models;
 
 public class Project
 {
-    
+    public Project( string title)
+    {
+        Id = Guid.NewGuid();
+        Title = title;
+    }
+
+    [Key]
+    public Guid Id { get; set; }
+    public string Title { get; set; }
 }
